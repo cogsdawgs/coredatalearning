22fbcba3c4328727034706d41a4a3311b0715940
Michael Coggins <user@Users-MacBook-Pro.local>
2016-09-20 15:54:39 -0500

    Initial Commit

---
+++ Core Data Model/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core Data Model
+//
+//  Created by Michael C on 9/20/16.
+//  Copyright © 2016 Michael C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
